696fe3e90e6cd0d003109867cd34ef62b817655a
arifeensifat01 <arifeen.sifat01@gmail.com>
2023-09-12 19:47:16 +0600

    create addUserValidationHandler

--- middlewares/common/users/userValidators.js
+++ middlewares/common/users/userValidators.js
@@ -50,3 +50,33 @@ const addUserValidators = [
       "Password must be at least 8 characters long & should contain at least 1 lowercase, 1 uppercase, 1 number & 1 symbol"
     ),
 ];
+
+
+const addUserValidationHandler = function (req, res, next) {
+    const errors = validationResult(req);
+    const mappedErrors = errors.mapped();
+    if (Object.keys(mappedErrors).length === 0) {
+      next();
+    } else {
+      // remove uploaded files
+      if (req.files.length > 0) {
+        const { filename } = req.files[0];
+        unlink(
+          path.join(__dirname, `/../public/uploads/avatars/${filename}`),
+          (err) => {
+            if (err) console.log(err);
+          }
+        );
+      }
+  
+      // response the errors
+      res.status(500).json({
+        errors: mappedErrors,
+      });
+    }
+  };
+  
+  module.exports = {
+    addUserValidators,
+    addUserValidationHandler,
+  };

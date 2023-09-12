3b1845466171f8c510e70e7400b6fba8735204c7
arifeensifat01 <arifeen.sifat01@gmail.com>
2023-09-12 19:35:53 +0600

    check mobile validator

--- middlewares/common/users/userValidators.js
+++ middlewares/common/users/userValidators.js
@@ -1,16 +1,47 @@
 // external imports
 const { check, validationResult } = require("express-validator");
+const createError = require("http-errors");
+const path = require("path");
+const { unlink } = require("fs");
+
+// internal imports
+const User = require("../../models/People");
 
 // add user
 const addUserValidators = [
-    check("name")
-      .isLength({ min: 1 })
-      .withMessage("Name is required")
-      .isAlpha("en-US", { ignore: " -" })
-      .withMessage("Name must not contain anything other than alphabet")
-      .trim(),
-    check("email")
-      .isEmail()
-      .withMessage("Invalid email address")
-      .trim()
+  check("name")
+    .isLength({ min: 1 })
+    .withMessage("Name is required")
+    .isAlpha("en-US", { ignore: " -" })
+    .withMessage("Name must not contain anything other than alphabet")
+    .trim(),
+  check("email")
+    .isEmail()
+    .withMessage("Invalid email address")
+    .trim()
+    .custom(async (value) => {
+      try {
+        const user = await User.findOne({ email: value });
+        if (user) {
+          throw createError("Email already is use!");
+        }
+      } catch (err) {
+        throw createError(err.message);
+      }
+    }),
+    check("mobile")
+    .isMobilePhone("bn-BD", {
+      strictMode: true,
+    })
+    .withMessage("Mobile number must be a valid Bangladeshi mobile number")
+    .custom(async (value) => {
+      try {
+        const user = await User.findOne({ mobile: value });
+        if (user) {
+          throw createError("Mobile already is use!");
+        }
+      } catch (err) {
+        throw createError(err.message);
+      }
+    }),
 ];

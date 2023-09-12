54eeda416b1bd7fc39fe4ee69da2ec650a9a616c
arifeensifat01 <arifeen.sifat01@gmail.com>
2023-09-12 18:49:09 +0600

    call the middleware function

--- middlewares/common/users/avatarUpload.js
+++ middlewares/common/users/avatarUpload.js
@@ -5,5 +5,20 @@
         1000000,
         "Only .jpg, jpeg or .png format allowed!"
       );
+
+      //call the middleware function
+      upload.any()(req. res, (err) => {
+        if (err) {
+            res.status(500).json({
+              errors: {
+                avatar: {
+                  msg: err.message,
+                },
+              },
+            });
+          } else {
+            next();
+          }
+      });
  }
  module.exports = avatarUpload;

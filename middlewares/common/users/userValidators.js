f0b479d3e3e5bec3ba460482a1061c8fdbbd4a35
arifeensifat01 <arifeen.sifat01@gmail.com>
2023-09-12 19:37:15 +0600

    check password validator

--- middlewares/common/users/userValidators.js
+++ middlewares/common/users/userValidators.js
@@ -44,4 +44,9 @@ const addUserValidators = [
         throw createError(err.message);
       }
     }),
+    check("password")
+    .isStrongPassword()
+    .withMessage(
+      "Password must be at least 8 characters long & should contain at least 1 lowercase, 1 uppercase, 1 number & 1 symbol"
+    ),
 ];

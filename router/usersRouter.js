b51e2f843d960198054d86863e2085fa344b64ab
arifeensifat01 <arifeen.sifat01@gmail.com>
2023-09-12 19:09:14 +0600

    add user

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -1,15 +1,26 @@
 //external imports
 const express = require("express");
+const { check } = require("express-validator");
 
 // internal imports
 const {getUsers} = require("../controller/usersController");
 const decorateHtmlResponse =require("../middlewares/common/decorateHtmlResponse");
+const avatarUpload = require("../middlewares/common/users/avatarUpload");
 
 
 const router =  express.Router();
 
 
-// login page
+// users page
 router.get("/",decorateHtmlResponse("Users"), getUsers);
+
+// add user
+router.post(
+    "/",
+    avatarUpload,
+    addUserValidators,
+    addUserValidationHandler,
+    addUser
+  );
 
 module.exports = router;
